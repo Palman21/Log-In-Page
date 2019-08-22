5a82b615b3f9a1593eeb4dfd4378de24fd575a99
Palman21 <53499730+Palman21@users.noreply.github.com>
2019-08-23 05:21:48 +0800

    Add files via upload

---
+++ photogallery.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Assignment 04 - Photo Gallery</title>
+	<h1 style="background-color:orange;" align="center">PalmanS Photo Gallery</h1></a>
+</head>
+<body style="background-color:black;">
+
+	<h1 style="background-color:white;" align="center"> List of Photos </h1></a>
+	<a href="Photo01.php"><center>Photo 1</a></center>
+	<a href="Photo02.php"><center>Photo 2</a></center>
+	<a href="Photo03.php"><center>Photo 3</a></center>
+	
+
+</body>
+<footer>
+	<a href="Log In Page.php">Log Out</a>
+</footer>
+</html>
