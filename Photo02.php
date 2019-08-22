4263e55a0eff2e2b52619aa5cbae031ae515679a
Palman21 <53499730+Palman21@users.noreply.github.com>
2019-08-23 05:24:29 +0800

    Add files via upload

---
+++ Photo02.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<style>
+
+</style>
+<head>
+	<title>Photo 1</title>
+	<h1 style="background-color:orange;" align="center">PalmanS Photo Gallery</h1></a>
+</head>
+<body style="background-color:black;">
+	<center><img src="Lukey002.jpg" width="500" height="500"></center>	
+	<a href="index.php" style="float: left"> Back to Home </a><a href="Photo03.php" style="float: right"> Next Page </a>
+
+	</body>
+	</html>
---
+++ Photo03.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<style>
+
+</style>
+<head>
+	<title>Photo 1</title>
+	<h1 style="background-color:orange;" align="center">PalmanS Photo Gallery</h1></a>
+</head>
+<body style="background-color:black;">
+	<center><img src="Lukey004.jpg" width="500" height="500"></center>	
+	<a href="index.php" style="float: left"> Back to Home </a>
+	</body>
+	</html>
